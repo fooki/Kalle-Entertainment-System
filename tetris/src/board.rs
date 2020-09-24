5e9f229f8d2c068dc518859e6810ee8eb0147768
Karl Johansson <karl.johansson@t2data.com>
2020-09-24 21:10:05 +0200

    Allow boards to return cells

--- tetris/src/board.rs
+++ tetris/src/board.rs
@@ -1,9 +1,11 @@
 use crate::tetris_block::TetrisBlock;
 use crate::figure::Figure;
 
+type BoardCells = [[TetrisBlock; 10]; 20];
+
 pub struct Board {
     current_figure: Option<Figure>,
-    cells: [[TetrisBlock; 10]; 20],
+    cells: BoardCells,
 }
 
 impl Board {
@@ -15,11 +17,19 @@ impl Board {
     pub fn set_current(&mut self, x: usize, y: usize, kind: TetrisBlock) {
         self.current_figure = Some(Figure::new(x, y, kind));
     }
+
+    pub fn cells(&self) -> &BoardCells {
+        &self.cells
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn empty_cells() -> BoardCells {
+        [[TetrisBlock::Empty; 10]; 20]
+    }
 
     #[test]
     fn test_new_does_not_crash() {
@@ -30,5 +40,11 @@ mod tests {
     fn test_set_current_adds_block() {
         let mut board = Board::new();
         board.set_current(5, 0, TetrisBlock::T);
+    }
+
+    #[test]
+    fn test_initial_board_returns_empty_cells() {
+        let board = Board::new();
+        assert_eq!(&empty_cells(), board.cells());
     }
 }
